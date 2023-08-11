6d5d9292803d518ab9ec29cc23b453a6be9121cb
ebasso <ebch.soares@outlook.com>
2023-08-11 11:02:14 -0300

    Início de desenvolvimento do projeto...

---
+++ App/Modelos/Usuario.cs
@@ -0,0 +1,26 @@
+﻿namespace App.Modelos;
+
+internal class Usuario
+{
+    private List<Usuario> nomes = new();
+    private List<Usuario> emails = new();
+    private List<Usuario> senhas = new();
+
+    public Usuario(string nome, string email, string senha)
+    {
+        Nome = nome;
+        Email = email;
+        Senha = senha;
+    }
+
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Senha { get; set; }
+
+    public void RegistrarUsuario(Usuario nome, Usuario email, Usuario senha)
+    {
+        nomes.Add(nome);
+        emails.Add(email);
+        senhas.Add(senha);
+    }
+}
--- App/Program.cs
+++ App/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using App.Modelos;
+using System.Runtime.CompilerServices;
+
+void Logo()
+{
+    Console.WriteLine("Seja bem vindo(a) ao IEJPN!");
+    Console.WriteLine("--- SISTEMA DE USUÁRIOS ---\n");
+}
+void Menu()
+{
+    Logo();
+    Console.WriteLine("Digite 1 para registrar um novo cadastro");
+    Console.WriteLine("Digite 2 para listar os usuários cadastrados");
+    Console.WriteLine("Digite 3 para exibir detalhes");
+    Console.WriteLine("Digite 0 para sair");
+
+    int opcao = int.Parse(Console.ReadLine()!);
+
+    switch (opcao)
+    {
+        case 1:
+            Console.WriteLine("Acessando a opção 1");
+            break;
+        case 2:
+            Console.WriteLine("Acessando a opção 2");
+                break;
+        case 3:
+            Console.WriteLine("Acessando a opção 3");
+            break;
+        case 0:
+            Console.WriteLine("Saindo o Sistema de Usuários...");
+            Thread.Sleep(1000);
+            break;
+    }
+}
+
+
+Menu();
